13cd4d24ad26a85c9719e57a9a27ef3731b51578
waleedahmad2 <84619161+waleedahmad2@users.noreply.github.com>
2024-08-29 11:08:30 -0700

    Update uploadRoutes.js

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,8 +1,15 @@
-// routes/uploadRoutes.js
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const cloudinary = require('../config/cloudinaryConfig');
+const { Readable } = require('stream');
+
+// Import the middleware functions
 const { upload, uploadImages } = require('../controllers/uploadController');
 
-router.post('/upload', upload,uploadImages);
+const storage = multer.memoryStorage(); // Use memory storage instead of disk storage
+const upload = multer({ storage }).array('images', 5);
+
+router.post('/upload', upload, uploadImages);
 
 module.exports = router;
