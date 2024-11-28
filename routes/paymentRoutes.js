8a8fafd4007364ddf0b3f18854a052bb80a44653
Waleed Ahmad <waleedahmad1806@gmail.com>
2024-11-29 02:49:06 +0500

    updated webhook controller

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // Define the route for creating a payment intent
 router.post('/create-payment-intent', createCustomerAndPaymentIntent);
 
-app.post('/webhook', bodyParser.raw({ type: 'application/json' }), handleStripeWebhook);
+router.post('/webhook', bodyParser.raw({ type: 'application/json' }), handleStripeWebhook);
 
 router.post('/withdraw', withdrawFunds);
 
