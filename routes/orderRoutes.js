25866f21579e49a39958cc44def10f38c5ce1529
Waleed Ahmad <waleedahmad1806@gmail.com>
2024-11-28 05:35:52 +0500

    updated create order route

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,12 +3,13 @@ const express = require('express');
 const multer = require('multer');
 const { storage } = require('../config/cloudinaryConfig');
 const {  createOrder,getOrders,getOrder, getPaymentsSummary } = require('../controllers/orderControllers');
+const { verifyToken } = require('../middleware/jwt');
 
 const upload = multer({ storage });
 
 const router = express.Router();
 
-router.post('/',createOrder );
+router.post('/',verifyToken,createOrder );
 router.get('/',getOrders );
 router.get("/:id",getOrder);
 router.get('/payments/summary', getPaymentsSummary);
